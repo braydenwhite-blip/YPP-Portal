73f319bfc5682212c37d5730914283de565ce266
wesley ho <wesley.ho@youthpassionproject.org>
2026-06-14 15:52:23 -0400

    fix: access inner array of timeline details object

--- app/(app)/admin/mentorship/gr/[documentId]/page.tsx
+++ app/(app)/admin/mentorship/gr/[documentId]/page.tsx
@@ -113,7 +113,7 @@ export default async function AdminGRDocumentDetailPage({ params }: Props) {
             gap: 12,
           }}
         >
-          {(timeline ?? []).map((phase: any) => (
+          {(timeline?.phases ?? []).map((phase: any) => (
             <div
               key={phase.phase}
               style={{
